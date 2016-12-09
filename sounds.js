75128dcd3badb1e0193df31afac80227d10f56fd
johnnydoubt <cesar.tailleur@gmail.com>
2016-12-09 10:12:38 +0100

    ajout des 24 scripts, son non definitif

---
+++ sounds.js
@@ -0,0 +1,194 @@
+$(function(){
+$("#1").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#2").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#3").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#4").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#5").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#6").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#7").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#8").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#9").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#10").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#11").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#12").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#13").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#14").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#15").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#16").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#17").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#18").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#19").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#20").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#21").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#22").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#23").click(function(){
+        var audio = $('audio')[0];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+$("#24").click(function(){
+        var audio = $('audio')[1];
+        if (audio.paused){
+            audio.play();
+        } else {
+            audio.pause();
+        }
+});
+});
