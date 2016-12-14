c6293899f70f974724ea83e1534eaeed6aa2ec8f
jethro13 <jeanjacques.barbier08@gmail.com>
2016-12-14 10:00:58 +0100

    ajout function mouvement souris

--- exampleSVG.js
+++ exampleSVG.js
@@ -4,3 +4,11 @@ function random(range){
 let oldCursor = {x:0, y:0};
 let svg;
 let cursor = {x:0, y:0};
+function onMouseMove(e){
+  var e = e || window.event;
+
+  if (e.pageX || e.pageY){
+    cursor.x = e.pageX;
+    cursor.y = e.pageY;
+  }
+};
