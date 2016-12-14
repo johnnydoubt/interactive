d73101fccc6b3a7b3e0733620f325e1e72ce7c32
jethro13 <jeanjacques.barbier08@gmail.com>
2016-12-14 13:59:12 +0100

    commentaire 2

--- js/cursor.js
+++ js/cursor.js
@@ -18,10 +18,12 @@ window.onload = function() {
      //définition taille canvas
      canvas.width  = window.innerWidth;
      canvas.height = window.innerHeight;
-     canvas.style = "margin:auto;";
+     //ajout du canvas au dom
      document.body.appendChild(canvas);
      c.fillStyle = "black";
-     c.fillRect (0, 0, canvas.width, canvas.height);      function Particle() {
+     c.fillRect (0, 0, canvas.width, canvas.height);
+     //function de création de particule
+     function Particle() {
        this.x = app.mouse.x;
        this.y = app.mouse.y;
        this.vx = Math.random() * 10 - 5;
@@ -33,24 +35,29 @@ window.onload = function() {
        this.life = 0;
        this.maxLife = Math.random() * 30 + 50;
        this.color = "hsla("+parseInt(Math.random()*360, 10)+" ,100%,50%, 0.2)";
-     }      Particle.prototype.draw = function () {
+     }
+     //function de dessin des particules
+     Particle.prototype.draw = function () {
        this.x += this.vx;
        this.y += this.vy;        if (Math.random() < 0.1) {
          this.vx = Math.random() * 10 - 5;
          this.vy = Math.random() * 10 - 5;
        }
-       this.life++;        if (this.life >= this.maxLife){
+       //Durée de vie particule
+       this.life++;
+       if (this.life >= this.maxLife){
          delete particles[this.id];
       }        c.fillStyle = this.color
-       c.fillRect (this.x, this.y, 10, 10);      };      setInterval (function() {
+       c.fillRect (this.x, this.y, 10, 10);      };
+       //appel function drawParticule
+       setInterval (function() {
        c.globalCompositeOperation = "source-over";
        c.fillStyle = "rgba(0,0,0,0.5)";
-       c.fillRect ( 0, 0, canvas.width, canvas.height);        for (var i = 0; i < particleNum; i++){
+       c.fillRect ( 0, 0, canvas.width, canvas.height);
+       for (var i = 0; i < particleNum; i++){
          new Particle();        }
        c.globalCompositeOperation = "lighter";
        for(var i in particles){
-       particles[i].draw();
-
-   }}, 30);
+       particles[i].draw();}}, 30);
 
  };
