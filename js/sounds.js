38f0e8cd3893184404b1121b3256a8d8b0ce07b0
johnnydoubt <cesar.tailleur@gmail.com>
2017-01-02 21:56:41 +0100

    all sounds here !

--- js/sounds.js
+++ js/sounds.js
@@ -1,199 +1,22 @@
+
+
 $(function() {
     $(".effects").mouseover(function() {
       let audio = new Audio('sounds/'+$(this).data("soundfile"));
         if (audio.paused) {
             audio.play();
+            $(audio).prop("volume", 0.1);
         } else {
             audio.pause();
         }
     });
     });
 
-    /*
-    $("#2").mouseover(function() {
-      let audio = new Audio('sounds/B.wav');
+    $(document).ready(function() {
+      let audio = new Audio('sounds/PROJETXY_OST.mp3');
         if (audio.paused) {
             audio.play();
         } else {
             audio.pause();
         }
     });
-    $("#3").mouseover(function() {
-      let audio = new Audio('sounds/C.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#4").mouseover(function() {
-      let audio = new Audio('sounds/D.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#5").mouseover(function() {
-      let audio = new Audio('sounds/E.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#6").mouseover(function() {
-      let audio = new Audio('sounds/F.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#7").mouseover(function() {
-      let audio = new Audio('sounds/G.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#8").mouseover(function() {
-      let audio = new Audio('sounds/H.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#9").mouseover(function() {
-      let audio = new Audio('sounds/J.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#10").mouseover(function() {
-      let audio = new Audio('sounds/K.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#11").mouseover(function() {
-        let audio = new Audio('sounds/L.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#12").mouseover(function() {
-        let audio = new Audio('sounds/M.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#13").mouseover(function() {
-        let audio = new Audio('sounds/N.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#14").mouseover(function() {
-        let audio = new Audio('sounds/O.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#15").mouseover(function() {
-        let audio = new Audio('sounds/P.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#16").mouseover(function() {
-        let audio = new Audio('sounds/Q.wav');
-        if (audio.pause) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#17").mouseover(function() {
-        let audio = new Audio('sounds/R.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#18").mouseover(function() {
-        let audio = new Audio('sounds/S.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#19").mouseover(function() {
-        let audio = new Audio('sounds/T.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#20").mouseover(function() {
-        let audio = new Audio('sounds/U.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#21").mouseover(function() {
-        let audio = new Audio('sounds/V.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#22").mouseover(function() {
-        let audio = new Audio('sounds/W.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#23").mouseover(function() {
-        let audio = new Audio('sounds/X.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-    $("#24").mouseover(function() {
-        let audio = new Audio('sounds/Y.wav');
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    });
-});
-
-*/
