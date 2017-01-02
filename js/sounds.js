d003cd025b72ffe71fe60ced9338af501519a7f5
johnnydoubt <cesar.tailleur@gmail.com>
2017-01-02 22:37:29 +0100

    new scripts for mobile

--- js/sounds.js
+++ js/sounds.js
@@ -1,4 +1,15 @@
-
+$('note').on('touchstart', function (e) {
+    'use strict'; //satisfy code inspectors
+    var link = $(this); //preselect the link
+    if (link.hasClass('hover')) {
+        return true;
+    } else {
+        link.addClass('hover');
+        $('note').not(this).removeClass('hover');
+        e.preventDefault();
+        return false; //extra, and to make sure the function has consistent return points
+    }
+});
 
 $(function() {
     $(".effects").mouseover(function() {
@@ -10,6 +21,18 @@ $(function() {
             audio.pause();
         }
     });
+    });
+
+$(function() {
+    $(".effects").on('touchstart', (function() {
+      let audio = new Audio('sounds/'+$(this).data("soundfile"));
+        if (audio.paused) {
+            audio.play();
+            $(audio).prop("volume", 0.1);
+        } else {
+            audio.pause();
+        }
+    }));
     });
 
     $(document).ready(function() {
