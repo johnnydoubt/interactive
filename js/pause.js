5c632b81fdbecd83901eb1ffb329f2fab1178a09
johnnydoubt <cesar.tailleur@gmail.com>
2017-01-29 18:50:25 +0100

    Menu on keydown added

---
+++ js/pause.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("keydown", function() {
+	if ( document.getElementById("myNav").style.width == "0%") {
+		document.getElementById("myNav").style.width = "100%";
+	} else {
+	document.getElementById("myNav").style.width = "0%"
+}
+
+});
